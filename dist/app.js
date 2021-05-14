d1cc4a63a8621117a7a3f1856258ef3447233a88
Alebacce <73042051+Alebacce@users.noreply.github.com>
2021-05-14 17:50:52 +0200

    Completed (implemented Teacher's corrections)

--- dist/app.js
+++ dist/app.js
@@ -12,9 +12,36 @@ var app = new Vue({
   data: {
     // HEADER
     // Menu list
-    headerMenu: ['Home', 'Rates', 'Testimonials', 'FAQ', 'Blog', 'Contact'],
+    headerMenu: [{
+      link: '#',
+      name: 'Home',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Rates',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Testimonials',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'FAQ',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Blog',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Contact',
+      isClicked: false
+    }],
     // Button
-    headerMenuBtn: 'FREE QUOTE',
+    headerMenuBtn: {
+      link: '#',
+      name: 'FREE QUOTE'
+    },
     // SOCIAL ICONS & LINKS
     socials: [// Facebook
     {
@@ -38,10 +65,87 @@ var app = new Vue({
     footerMenuTitle_1: 'Avada Movers',
     footerMenuTitle_2: 'Recent Posts',
     // Menu lists
-    footerMenu_1: ['Home', 'Rates', 'Testimonials', 'Blog', 'Free Quote'],
-    footerMenu_2: ['Heading Out To College?', 'Moving Your Buisiness?', 'Outstanding Quality', 'Cost of Moving', 'Best Moving Tips'],
+    footerMenu_1: [{
+      link: '#',
+      name: 'Home',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Rates',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Testimonials',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Blog',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Free Quote',
+      isClicked: false
+    }],
+    footerMenu_2: [{
+      link: '#',
+      name: 'Heading Out To College?',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Moving Your Buisiness?',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Outstanding Quality',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Cost of Moving',
+      isClicked: false
+    }, {
+      link: '#',
+      name: 'Best Moving Tips',
+      isClicked: false
+    }],
     //Copyright and Legal stuffs links
-    copyAndLegal: ['© Copyright 2012 - 2020', 'Avada Theme by ThemeFusion', 'All Rights Reserved', 'Powered by Wordpress']
+    copyAndLegal: [{
+      link: '#',
+      name: '© Copyright 2012 - 2020'
+    }, {
+      link: '#',
+      name: 'Avada Theme by ThemeFusion'
+    }, {
+      link: '#',
+      name: 'All Rights Reserved'
+    }, {
+      link: '#',
+      name: 'Powered by Wordpress'
+    }, {
+      link: '#',
+      name: 'Free Quote'
+    }],
+    // FLAGS
+    // thisLink saves the index of link in the header
+    thisLink: 0,
+    // thisLink saves the index of link in the footer list 1
+    thisFooterLink1: 0,
+    // thisLink saves the index of link in the footer list 2
+    thisFooterLink2: -1
+  },
+  methods: {
+    // Functions to activate one link per time
+    clicked: function clicked(index) {
+      this.thisLink = index;
+      this.headerMenu[index].isClicked = !this.headerMenu[index].isClicked;
+    },
+    clickedFooter1: function clickedFooter1(index) {
+      this.thisFooterLink1 = index;
+      this.footerMenu_1[index].isClicked = !this.footerMenu_1[index].isClicked;
+    },
+    clickedFooter2: function clickedFooter2(index) {
+      this.thisFooterLink2 = index;
+      this.footerMenu_2[index].isClicked = !this.footerMenu_2[index].isClicked;
+    }
   }
 });
 
--- src/app.js
+++ src/app.js
@@ -5,16 +5,43 @@ var app = new Vue ({
         // HEADER
         // Menu list
         headerMenu: [
-        'Home', 
-        'Rates', 
-        'Testimonials',
-        'FAQ', 
-        'Blog', 
-        'Contact'
+            {
+                link: '#',
+                name: 'Home',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Rates',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Testimonials',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'FAQ',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Blog',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Contact',
+                isClicked: false
+            },
         ],
 
         // Button
-        headerMenuBtn: 'FREE QUOTE',
+        headerMenuBtn: {
+            link: '#',
+            name: 'FREE QUOTE'
+        },
 
         // SOCIAL ICONS & LINKS
         socials: [
@@ -47,28 +74,110 @@ var app = new Vue ({
         
         // Menu lists
         footerMenu_1: [
-                'Home',
-                'Rates',
-                'Testimonials',
-                'Blog',
-                'Free Quote'
+            {
+                link: '#',
+                name: 'Home',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Rates',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Testimonials',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Blog',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Free Quote',
+                isClicked: false
+            },
+
         ],
 
         footerMenu_2: [
-                'Heading Out To College?',
-                'Moving Your Buisiness?',
-                'Outstanding Quality',
-                'Cost of Moving',
-                'Best Moving Tips'
+            {
+                link: '#',
+                name: 'Heading Out To College?',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Moving Your Buisiness?',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Outstanding Quality',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Cost of Moving',
+                isClicked: false
+            },
+            {
+                link: '#',
+                name: 'Best Moving Tips',
+                isClicked: false
+            },
         ],
 
         //Copyright and Legal stuffs links
         copyAndLegal: [
-            '© Copyright 2012 - 2020',
-            'Avada Theme by ThemeFusion',
-            'All Rights Reserved',
-            'Powered by Wordpress'
-        ]
+            {
+                link: '#',
+                name: '© Copyright 2012 - 2020'
+            },
+            {
+                link: '#',
+                name: 'Avada Theme by ThemeFusion'
+            },
+            {
+                link: '#',
+                name: 'All Rights Reserved'
+            },
+            {
+                link: '#',
+                name: 'Powered by Wordpress'
+            },
+            {
+                link: '#',
+                name: 'Free Quote'
+            },
+        ],
+
+        // FLAGS
+        // thisLink saves the index of link in the header
+        thisLink: 0,
+        // thisLink saves the index of link in the footer list 1
+        thisFooterLink1: 0,
+        // thisLink saves the index of link in the footer list 2
+        thisFooterLink2: -1,
     },
 
+    methods: {
+        // Functions to activate one link per time
+        clicked(index) {
+            this.thisLink = index;
+            this.headerMenu[index].isClicked = !this.headerMenu[index].isClicked;
+        },
+
+        clickedFooter1(index) {
+            this.thisFooterLink1 = index;
+            this.footerMenu_1[index].isClicked = !this.footerMenu_1[index].isClicked;
+        },
+        
+        clickedFooter2(index) {
+            this.thisFooterLink2 = index;
+            this.footerMenu_2[index].isClicked = !this.footerMenu_2[index].isClicked;
+        } 
+    }
 });
